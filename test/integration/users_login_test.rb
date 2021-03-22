18cb1dcd44dc118c1cd50812377060729261d903
HayashiTakato <hayashi08841998@gmail.com>
2021-03-22 21:55:48 +0900

    Add:is_logged_in?メソッドをテストに追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -13,6 +13,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     follow_redirect!
     assert_template "users/show"
+    assert is_logged_in?
   end
 
   test "login with invalid informartion" do
@@ -21,6 +22,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params: {session: {email: "", password: ""}}
     assert_template "sessions/new"
     assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+    assert_not is_logged_in?
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: {session: {email: @user.email,
+                                        password: "invalid"}}
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    assert_not is_logged_in?
     get root_path
     assert flash.empty?
   end
