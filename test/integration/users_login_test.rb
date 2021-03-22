31135dabbefa8d6b756eff8bb69a89dae3cc53eb
HayashiTakato <hayashi08841998@gmail.com>
2021-03-22 22:40:11 +0900

    Add:ログアウト機能を追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,21 +10,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template "users/show"
-    assert is_logged_in?
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
   end
 
   test "login with invalid informartion" do
     get login_path
     assert_template "sessions/new"
     post login_path, params: {session: {email: "", password: ""}}
+    assert_not is_logged_in?
     assert_template "sessions/new"
     assert_not flash.empty?
     get root_path
     assert flash.empty?
-    assert_not is_logged_in?
   end
 
   test "login with valid email/invalid password" do
@@ -32,9 +35,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template "sessions/new"
     post login_path, params: {session: {email: @user.email,
                                         password: "invalid"}}
+    assert_not is_logged_in?
     assert_template "sessions/new"
     assert_not flash.empty?
-    assert_not is_logged_in?
     get root_path
     assert flash.empty?
   end
