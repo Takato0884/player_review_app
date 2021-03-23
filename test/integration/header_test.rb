9c3b64839551c728aa42167830ffff8426995c92
HayashiTakato <hayashi08841998@gmail.com>
2021-03-23 15:00:58 +0900

    Change:ログアウトで永続cookiesを破棄するように変更

--- test/integration/header_test.rb
+++ test/integration/header_test.rb
@@ -6,27 +6,34 @@ class HeaderTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "header when not logging in" do
-    get root_path
-    assert_select "a[href=?]", root_path
-    assert_select "a[href=?]", games_path
-    assert_select "a[href=?]", grampus_players_path
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
-  end
+  # test "header when not logging in" do
+  #   get root_path
+  #   assert_select "a[href=?]", root_path
+  #   assert_select "a[href=?]", games_path
+  #   assert_select "a[href=?]", grampus_players_path
+  #   assert_select "a[href=?]", login_path
+  #   assert_select "a[href=?]", logout_path, count: 0
+  # end
 
-  test "header when logging in" do
+  test "header when logging in/not logging in" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
     follow_redirect!
-    assert_template "users/show"
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", games_path
     assert_select "a[href=?]", grampus_players_path
     assert_select "a[href=?]", login_path, count: 0
     #設定のurlに関するテストをここに追加
     assert_select "a[href=?]", logout_path
+
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", games_path
+    assert_select "a[href=?]", grampus_players_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
   end
 
 
