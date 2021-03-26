5e6971dbb53b545f6d77d505135214625ba67dad
HayashiTakato <hayashi08841998@gmail.com>
2021-03-26 22:55:50 +0900

    Add:session#newのsignup_pathに関するテストを追加

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,4 +6,9 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should be signup_path" do
+    get login_path
+    assert_select "a[href=?]", signup_path
+  end
+
 end
