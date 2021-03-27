cbf1650596524885ac288079e97b7ef5d645437c
HayashiTakato <hayashi08841998@gmail.com>
2021-03-27 22:56:42 +0900

    Add:Reviewモデルのテストを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,9 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
   belongs_to :grampus_player
+  validates :user_id, presence: true
+  validates :game_id, presence: true
+  validates :grampus_player_id, presence: true
+  VALID_EMAIL_REGEX = /[1-5]/i
+  validates :score, presence:true, format: {with: VALID_EMAIL_REGEX}
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @game = games(:one)
+    @user = users(:michael)
+    @grampus_player = grampus_players(:langerak)
+    # @review = Review.new(score: "5", user_id: "1", grampus_player_id: "1", game_id: "1")
+    @review = @game.reviews.build(score: "5", user_id: @user.id, grampus_player_id: @grampus_player.id)
+  end
+
+  test "should be valid" do
+    assert @review.valid?
+  end
+
+  test "user_id should be present" do
+    @review.user_id = nil
+    assert_not @review.valid?
+  end
+
+  test "game_id should be present" do
+    @review.game_id = nil
+    assert_not @review.valid?
+  end
+
+  test "grampus_player_id should be present" do
+    @review.grampus_player_id = nil
+    assert_not @review.valid?
+  end
+
+  test "score should be present" do
+    @review.score = nil
+    assert_not @review.valid?
+  end
+
+  test "score should be any of [1-5]" do
+    @review.score = 6
+    assert_not @review.valid?
+  end
+
 end
