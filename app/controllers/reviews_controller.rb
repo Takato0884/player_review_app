e934b8510e5e214f58ac7e355a112ad322a4567e
HayashiTakato <hayashi08841998@gmail.com>
2021-03-30 21:44:42 +0900

    Add:reviewの削除機能を追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @review = current_user.reviews.build(review_params)
@@ -16,11 +17,19 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+    @review.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to game_path(1)
   end
 
   private
 
     def review_params
       params.require(:review).permit(:score, :user_id, :game_id, :grampus_player_id)
+    end
+
+    def correct_user
+      @review = current_user.reviews.find_by(id: params[:id])
+      redirect_to root_url if @review.nil?
     end
 end
--- test/integration/reviews_test.rb
+++ test/integration/reviews_test.rb
@@ -3,23 +3,24 @@ require 'test_helper'
 class ReviewsTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:other)
+    @other = users(:other)
+    @michael = users(:michael)
   end
 
   test "create vaild review without login" do
     assert_no_difference "Review.count" do
       post reviews_path, params: { review: {score: 5},
-                                    user_id: @user.id,
+                                    user_id: @other.id,
                                     game_id: 1,
                                     grampus_player_id: 1}
     end
   end
 
   test "create valid review with login" do
-    log_in_as(@user)
+    log_in_as(@other)
     assert_difference "Review.count", 1 do
       post reviews_path, params: { review: {score: 5},
-                                    user_id: @user.id,
+                                    user_id: @other.id,
                                     game_id: 1,
                                     grampus_player_id: 1}
     end
@@ -29,10 +30,10 @@ class ReviewsTest < ActionDispatch::IntegrationTest
   end
 
   test "create invalid review with login" do
-    log_in_as(@user)
+    log_in_as(@other)
     assert_no_difference "Review.count" do
       post reviews_path, params: { review: {score: "invalid"},
-                                    user_id: @user.id,
+                                    user_id: @other.id,
                                     game_id: 1,
                                     grampus_player_id: 1}
     end
@@ -41,4 +42,33 @@ class ReviewsTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
   end
 
+  test "destroy review with correct_user" do
+    log_in_as(@michael)
+    review = reviews(:one)
+    assert_difference "Review.count", -1 do
+      delete review_path(review)
+    end
+    follow_redirect!
+    assert_template "games/show"
+    assert_not flash.empty?
+  end
+
+  test "destroy review with incorrect_user" do
+    log_in_as(@other)
+    review = reviews(:one)
+    assert_no_difference "Review.count" do
+      delete review_path(review)
+    end
+    follow_redirect!
+    assert_template "static_pages/home"
+  end
+
+  test "destroy review without login" do
+    review = reviews(:one)
+    assert_no_difference "Review.count" do
+      delete review_path(review)
+    end
+    follow_redirect!
+    assert_template "sessions/new"
+  end
 end
