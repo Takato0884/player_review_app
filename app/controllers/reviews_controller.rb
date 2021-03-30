a403a843ed85a8070ccd749843581c42341c7fb3
HayashiTakato <hayashi08841998@gmail.com>
2021-03-30 19:31:53 +0900

    Add:ログインユーザのみがreviewのcreateとdestroyを呼び出せるようにした

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
 
   def create
     @review = current_user.reviews.build(review_params)
--- test/integration/reviews_test.rb
+++ test/integration/reviews_test.rb
@@ -3,17 +3,42 @@ require 'test_helper'
 class ReviewsTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user = users(:other)
   end
 
-  # test "combination of [:user_id, :game_id, :grampus_player_id] should be unique" do
-  #
-  #   assert_no_difference "Review.count" do
-  #     log_in_as(@user)
-  #     post reviews_path, params: { review: {score: 5},
-  #                                   user_id: @user.id,
-  #                                   game_id: 1,
-  #                                   grampus_player_id: 1}
-  #   end
-  # end
+  test "create vaild review without login" do
+    assert_no_difference "Review.count" do
+      post reviews_path, params: { review: {score: 5},
+                                    user_id: @user.id,
+                                    game_id: 1,
+                                    grampus_player_id: 1}
+    end
+  end
+
+  test "create valid review with login" do
+    log_in_as(@user)
+    assert_difference "Review.count", 1 do
+      post reviews_path, params: { review: {score: 5},
+                                    user_id: @user.id,
+                                    game_id: 1,
+                                    grampus_player_id: 1}
+    end
+    follow_redirect!
+    assert_template "games/show"
+    assert_not flash.empty?
+  end
+
+  test "create invalid review with login" do
+    log_in_as(@user)
+    assert_no_difference "Review.count" do
+      post reviews_path, params: { review: {score: "invalid"},
+                                    user_id: @user.id,
+                                    game_id: 1,
+                                    grampus_player_id: 1}
+    end
+    follow_redirect!
+    assert_template "games/show"
+    assert_not flash.empty?
+  end
+
 end
